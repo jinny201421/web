b51e0b7aff71f49348c34a25da6588e4f2d29927
jinny201421 <songlan201421@gmail.com>
2025-09-06 17:02:18 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,13 +1,29 @@
 let btn1 = document.getElementById('button1');
 let btn2 = document.getElementById('button2');
+let btn3 = document.getElementById('button3');
+let btn4 = document.getElementById('button4');
+
 
 
 function func1() {
     alert('나락!!\n아빠는 삐쳤다고 한다...\n쩝쩝..{속마음}이런 배은 망덕한');
 }
+
 function func2() {
     alert('나락!!\n엄마는 삐쳤다고 한다...\n밥 없어! 용돈 없어!!');
 }
 
+function func3() {
+    alert('You are a "GAY"/"LESBIAN"!!!');
+}
+
+function func4() {
+    alert('Congrats!! You are a heterosexual!!');
+}
+
+
+
 btn1.onclick = func1;
 btn2.onclick = func2;
+btn3.onclick = func3;
+btn4.onclick = func4;
