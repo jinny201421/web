67346a2fd7e3747c7ba1a80380a37a28747fae5e
jinny201421 <songlan201421@gmail.com>
2025-09-06 16:52:42 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let btn1 = document.getElementById('button1');
+let btn2 = document.getElementById('button2');
+
+
+function func1() {
+    alert('나락!!\n아빠는 삐쳤다고 한다...\n쩝쩝..{속마음}이런 배은 망덕한');
+}
+function func2() {
+    alert('나락!!\n엄마는 삐쳤다고 한다...\n밥 없어! 용돈 없어!!');
+}
+
+btn1.onclick = func1;
+btn2.onclick = func2;
